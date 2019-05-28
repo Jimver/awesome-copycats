d7d54638b2455e9cbfa96304b0a757ca92d7cf06
Jim Verheijde <j.verheijde@intermax.nl>
2019-05-28 22:24:44 +0200

    Add nm applet on start

--- rc.lua
+++ rc.lua
@@ -58,6 +58,7 @@ local function run_once(cmd_arr)
 end
 
 run_once({"blueman-applet"})
+run_once({"nm-applet"})
 
 -- run_once({ "urxvtd", "unclutter -root" }) -- entries must be separated by commas
 
