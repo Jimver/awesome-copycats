b6b336c4b1fcf69371d0bd61e41961d10dbeaf43
Jim Verheijde <jim@polygon.nl>
2019-08-08 17:35:21 +0200

    Start compton with experimental backend

--- rc.lua
+++ rc.lua
@@ -66,7 +66,7 @@ end
 -- run_once({"redshift-gtk"})
 run_once({"nvidia-settings --load-config-only"})
 run_once({"fusuma"})
-run_once({"compton"})
+run_once({"compton --experimental-backends"})
 run_once({"dex -a -s /etc/xdg/autostart/:~/.config/autostart/"})
 run_once({"systemctl --user import-environment PATH DBUS_SESSION_BUS_ADDRESS"})
 run_once({"systemctl --no-block --user start xsession.target"})
