8342896650bf961178f246ed97f299e71363cca9
Jim Verheijde <jim@polygon.nl>
2019-08-22 14:09:52 +0200

    Have 9 tags

--- rc.lua
+++ rc.lua
@@ -117,7 +117,7 @@ local guieditor    = "kate"
 local scrlocker    = "i3lock-fancy-rapid 7 3"
 
 awful.util.terminal = terminal
-awful.util.tagnames = { "1", "2", "3", "4", "5" }
+awful.util.tagnames = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.floating,
